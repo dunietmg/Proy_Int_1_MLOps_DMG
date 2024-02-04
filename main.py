02c67a21d9f73991e201b46249e72c74c609ba39
Duniet Marrero García <dunietmg@gmail.com>
2024-02-04 05:30:15 -0300

    Actualizacion de archivos

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 
+# LIBRERÍAS
+
 from typing import Union, List
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-import pandas as pd
-from dateutil import parser
-import pyarrow.parquet as pq
 import os
 import string
 import re
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from dateutil import parser
+import pandas as pd
+import pyarrow.parquet as pq
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from fastapi.encoders import jsonable_encoder
 
-
-
-# Creacion de una aplicacion FastApi
 
 # ejecutar: uvicorn main:app --reload   =>para cargar en el servidor
 
-
 app = FastAPI()
 
-# ------- FUNCION de bienvenida ----------
+#  => FUNCION INICIO 
+
 @app.get("/")
 def read_root():
     welcome_message = "Bienvenido al Proyecto Integrador Nro.1 de la Etapa de Labs en Henry. Autor: Duniet Marrero García. "
@@ -31,86 +30,84 @@ def read_root():
 
 
 
-# ------- FUNCION developer ----------
+# => FUNCION DEVELOPER
 
 @app.get("/developer/{desarrollador}")
 def developer(desarrollador: str):
 
-    # Lee los archivos parquet de la carpeta data
-    # Obtén la ruta del directorio actual del script
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    path_to_parquet = os.path.join(current_directory, 'data', 'df_games_etl.parquet')
-    df_games_etl = pq.read_table(path_to_parquet).to_pandas()
+    path_to_parquet = os.path.join(current_directory, 'data', 'df_steam_games.parquet')
+    df_steam_games = pq.read_table(path_to_parquet).to_pandas()
     
-    # Filtrar el DataFrame por la empresa desarrolladora
-    df_desarrollador = df_games_etl[df_games_etl['developer'] == desarrollador].copy()
+    # Filtra el dataframe por la empresa desarrolladora
+    df_desarrollador = df_steam_games[df_steam_games['developer'] == desarrollador].copy()
 
     def obtener_anio(fecha):
         try:
-            # Intentar convertir la fecha al formato de fecha
+            # Convierte la fecha al formato correcto
             fecha_obj = parser.parse(fecha)
             return fecha_obj.year
         except:
-            # Si no se puede convertir, retornar un valor nulo o manejarlo según sea necesario
             return None
 
-    # Crear la columna "anio" extrayendo el año de la columna "release_date"
+    # Crea la columna "anio" a partir de la columna "release_date"
     df_desarrollador['anio'] = df_desarrollador['release_date'].apply(obtener_anio).astype('Int64')
 
-    # Contar la cantidad de items por año
+    # Cantidad de items por año
     cantidad_items_por_año = df_desarrollador.groupby('anio').size().reset_index(name='cantidad_items')
 
-    # Contar la cantidad de items gratuitos por año
+    # Cantidad de items gratuitos por año
     cantidad_items_gratuitos_por_año = (df_desarrollador[df_desarrollador['es_gratis']].groupby('anio').size().reset_index(name='cantidad_items_gratuitos')).astype('Int64')
 
-    # Fusionar los DataFrames para obtener la cantidad total y gratuita por año
+    # Une los dataframes 
     resultado = pd.merge(cantidad_items_por_año, cantidad_items_gratuitos_por_año, on='anio', how='left').fillna(0)
 
-    # Calcular el porcentaje de contenido gratuito por año
+    # Porcentaje de contenido gratuito por año
     resultado['porcentaje_gratuito'] = ((resultado['cantidad_items_gratuitos'] / resultado['cantidad_items']) * 100).round(2)
 
-    # Convertir el resultado a formato JSON para que pueda ser retornado por FastAPI
+    # Convierte el resultado a formato JSON
     resultado_json = resultado.to_dict(orient='records')
 
     return resultado_json
 
-# ------- FUNCION userdata ----------
+# => FUNCION USERDATA
 
 @app.get("/userdata/{user_id}")
 def userdata(user_id: str):
 
-    # Lee los archivos parquet de la carpeta data
+    # Lee los archivos parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    path_to_parquet = os.path.join(current_directory, 'data', 'df_gastos_items.parquet')
-    df_gastos_items = pq.read_table(path_to_parquet).to_pandas()
+    path_to_parquet = os.path.join(current_directory, 'data', 'df_items_userdata.parquet')
+    df_items_userdata = pq.read_table(path_to_parquet).to_pandas()
     
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    path_to_parquet = os.path.join(current_directory, 'data', 'df_reviews_etl.parquet')
-    df_reviews_etl = pq.read_table(path_to_parquet).to_pandas()
+    path_to_parquet = os.path.join(current_directory, 'data', 'df_reviews_userdata.parquet')
+    df_reviews_userdata = pq.read_table(path_to_parquet).to_pandas()
     
 
     # Filtra por el usuario de interés
-    usuario = df_reviews_etl[df_reviews_etl['user_id'] == user_id]
+    usuario = df_reviews_userdata[df_reviews_userdata['user_id'] == user_id]
     
     if usuario.empty:
         raise HTTPException(status_code=404, detail="User not found")
 
-    # Convertir user_id a tipo string para asegurarse de que coincida con el tipo de datos en los DataFrames
+    # Convierte user_id a tipo string 
     user_id = str(user_id)
 
-    # Calcula la cantidad de dinero gastado para el usuario de interés
-    cantidad_dinero = df_gastos_items[df_gastos_items['user_id'] == user_id]['price'].iloc[0]
+    # Cantidad de dinero gastado por el usuario 
+    cantidad_dinero = df_items_userdata[df_items_userdata['user_id'] == user_id]['price'].iloc[0]
     
-    # Busca el count_item para el usuario de interés
-    count_items = df_gastos_items[df_gastos_items['user_id'] == user_id]['items_count'].iloc[0]
+    # Cantidad de item para el usuario 
+    count_items = df_items_userdata[df_items_userdata['user_id'] == user_id]['items_count'].iloc[0]
 
-    # Calcula el total de recomendaciones realizadas por el usuario de interés
+    # Total de recomendaciones realizadas por el usuario
     total_recomendaciones = usuario['recommend'].sum()
     
-    # Calcula el total de reviews realizada por todos los usuarios
-    total_reviews = len(df_reviews_etl['user_id'].unique())
+    # Total de reviews realizados por todos los usuarios
+    total_reviews = len(df_reviews_userdata['user_id'].unique())
     
-    # Calcula el porcentaje de recomendaciones realizadas por el usuario de interés
+    # Porcentaje de recomendaciones realizadas por el usuario
     porcentaje_recomendaciones = (total_recomendaciones / total_reviews) * 100
 
     return {
@@ -119,116 +116,119 @@ def userdata(user_id: str):
         'total_items': int(count_items)
     }
 
-# ------- FUNCION user_for_genre ----------
+
+# => FUNCION USER_FOR_GENRE
 
 @app.get("/user_for_genre/{genre}", response_model=dict)
 def user_for_genre(genre: str):
-    # Lee el archivo parquet de la carpeta data
+
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
     
-    # Cargar datos de juegos y géneros
-    path_to_games_parquet = os.path.join(current_directory, 'data', 'df_games_genres.parquet')
-    df_games_genres = pq.read_table(path_to_games_parquet).to_pandas()
+    # Carga los datos de juegos y géneros
+    path_to_games_parquet = os.path.join(current_directory, 'data', 'df_games_user_genre.parquet')
+    df_games_user_genre = pq.read_table(path_to_games_parquet).to_pandas()
     
-    # Cargar datos de usuarios y horas jugadas
-    path_to_users_parquet = os.path.join(current_directory, 'data', 'df_users_horas.parquet')
-    df_users_horas = pq.read_table(path_to_users_parquet).to_pandas()
+    # Carga los datos de usuarios y horas jugadas
+    path_to_users_parquet = os.path.join(current_directory, 'data', 'df_user_horas_juego.parquet')
+    df_user_horas_juego = pq.read_table(path_to_users_parquet).to_pandas()
 
-    # Unir ambos dataframes
-    df_genres_hours = df_games_genres.merge(df_users_horas, on='item_id', how='right')
+    # Une ambos dataframes
+    df_genres_hours = df_games_user_genre.merge(df_user_horas_juego, on='item_id', how='right')
 
-    # Filtrar el DataFrame resultante para obtener solo las filas relacionadas con el género dado
+    # Filtra el dataframe para obtener solo las filas relacionadas con el género dado
     df_filtered = df_genres_hours[df_genres_hours['genres'] == genre]
 
     if df_filtered.empty:
         raise HTTPException(status_code=404, detail="No data found for the given genre")
 
-    # Encontrar el usuario que acumula más horas jugadas para el género dado
+    # Encuentra el usuario que acumula más horas jugadas para el género
     max_user = df_filtered.groupby('user_id')['playtime_forever'].sum().idxmax()
 
-    # Filtrar el DataFrame para obtener solo las filas relacionadas con el usuario que acumula más horas
+    # Filtra el dataframe para obtener solo las filas relacionadas con el usuario que acumula más horas
     df_user_max_hours = df_filtered[df_filtered['user_id'] == max_user]
 
-    # Agrupar por año y sumar las horas jugadas
+    # Agrupa por año y suma las horas jugadas
     horas_por_anio = df_user_max_hours.groupby('anio')['playtime_forever'].sum()
 
-    # Construir el diccionario de resultados
+    # Construiye el diccionario de resultados
     result_dict = {
         "Usuario con más horas jugadas para Género X": max_user,
         "Horas jugadas": [{"Año": int(year), "Horas": int(hours)} for year, hours in horas_por_anio.reset_index().to_dict(orient='split')['data']]
     }
 
-    return result_dict
+    result_json = jsonable_encoder(result_dict)
+    return JSONResponse(content=result_json)
 
-# ------- FUNCION best_developer_year ----------
+    
+
+# => FUNCION BEST_DEVELOPER_YEAR
 
 
-# Definir la ruta de FastAPI para la función best_developer_year
-@app.get("/best_developer/{year}", response_model=List[dict])
+@app.get("/best_developer_year/{anio}", response_model=List[dict])
 
-def best_developer_year(year: int):
+def best_developer_year(anio: int):
 
-    # Lee el archivo parquet de la carpeta data
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
     path_to_parquet = os.path.join(current_directory, 'data', 'df_best_developer_anio.parquet')
     df_best_developer_anio = pq.read_table(path_to_parquet).to_pandas()
 
-    # Filtra el DataFrame para el año dado y donde recommend es True y sentiment_analysis es positivo
-    df_filtered = df_best_developer_anio[(df_best_developer_anio['anio'] == year) &
+    # Filtra el dataframe para el año dado y donde recommend es True y sentiment_analysis es positivo
+    df_filtered = df_best_developer_anio[(df_best_developer_anio['anio'] == anio) &
                                          (df_best_developer_anio['recommend'] == True) &
                                          (df_best_developer_anio['sentiment_analysis'] > 1)]
 
     if df_filtered.empty:
         return None
 
-    # Agrupar por desarrollador y contar la cantidad de juegos recomendados
+    # Agrupa por desarrollador y cuenta la cantidad de juegos recomendados
     top_developers = df_filtered.groupby('developer')['recommend'].sum().nlargest(3)
 
-    # Construir el resultado como una lista de diccionarios
+    # Construye el resultado como una lista de diccionarios
     result = [{"Top {}".format(i + 1): developer} for i, (developer, _) in enumerate(top_developers.items())]
 
     return result
 
 
-# ------- FUNCION developer_reviews_analysis ----------
+# => FUNCION DEVELOPER_REVIEWS_ANALYSIS
 
-# Definir la ruta de FastAPI para la función developer_reviews_analysis
 @app.get("/developer-reviews-analysis/{desarrollador}")
 def developer_reviews_analysis_endpoint(desarrollador: str):
 
-    # Lee el archivo parquet de la carpeta data
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    path_to_parquet = os.path.join(current_directory, 'data', 'df_developer_anio.parquet')
-    df_developer_anio = pq.read_table(path_to_parquet).to_pandas()
+    path_to_parquet = os.path.join(current_directory, 'data', 'df_developer_review.parquet')
+    df_developer_review = pq.read_table(path_to_parquet).to_pandas()
     
 
-    # Filtra el DataFrame para el desarrollador dado
-    df_filtered = df_developer_anio[df_developer_anio['developer'] == desarrollador]
+    # Filtra el dataframe para el desarrollador dado
+    df_filtered = df_developer_review[df_developer_review['developer'] == desarrollador]
 
     
     if df_filtered.empty:
         raise HTTPException(status_code=404, detail=f"No se encontraron registros para el desarrollador {desarrollador}")
 
-    # Agrupar por análisis de sentimiento y contar la cantidad de registros
+    # Agrupa por análisis de sentimiento y cuenta la cantidad de registros
     analysis_counts = df_filtered.groupby('sentiment_analysis').size().to_dict()
 
-    # Construir el resultado como un diccionario con una lista
+    # Construye el resultado como un diccionario con una lista
     result = {desarrollador: [f'Negative = {analysis_counts.get(0, 0)}', f'Positive = {analysis_counts.get(2, 0)}']}
 
     return result
 
 
-# ------- ML RECOMENDACION - Títulos similares ----------
+# => ML MODELO DE RECOMENDACION - TITULOS DE JUEGOS SIMILARES
 
 @app.get("/recomendacion_juego/{titulo}")
 def get_recomendacion_juego(titulo: str):
     
-    # Lee el archivo parquet de la carpeta data
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
     path_to_parquet = os.path.join(current_directory, 'data', 'df_mod_rec_1.parquet')
     df_mod_rec_1 = pq.read_table(path_to_parquet).to_pandas()
 
-    # Configuración de TF-IDF
+    # Configura TF-IDF
     tfidf = TfidfVectorizer(stop_words='english')
     df_mod_rec_1['ntags'] = df_mod_rec_1['ntags'].fillna('')
     tfidf_matrix = tfidf.fit_transform(df_mod_rec_1['ntags'])
@@ -237,58 +237,59 @@ def get_recomendacion_juego(titulo: str):
     indices = pd.Series(df_mod_rec_1.index, index=df_mod_rec_1['app_name']).drop_duplicates()
 
     try:
-        # Obtener el índice del juego en la matriz de similitud coseno
+        # Índice del juego en la matriz de similitud coseno
         idx = indices[titulo]
 
-        # Obtener las puntuaciones de similitud para el juego
+        # Puntuaciones de similitud para el juego
         sim_scores = list(enumerate(cosine_sim[idx]))
 
-        # Ordenar las puntuaciones de similitud por orden descendente
+        # Puntuaciones de similitud por orden descendente
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-        # Obtener los índices de los 5 juegos más similares
+        # Índices de los 5 juegos más similares
         game_indices = [i[0] for i in sim_scores[1:6]]
 
-        # Obtener los títulos de los 5 juegos más similares
+        # Títulos de los 5 juegos más similares
         recommendations = df_mod_rec_1['app_name'].iloc[game_indices]
 
-        return JSONResponse(content={'title': titulo, 'recommendations': recommendations.tolist()})
+        result_json = jsonable_encoder({'TÍTULO': titulo, 'RECOMENDACIONES DE JUEGOS: ': recommendations.tolist()})
+        return JSONResponse(content=result_json)
 
     except KeyError:
         raise HTTPException(status_code=404, detail=f'El juego {titulo} no se encuentra en el DataFrame.')
     
 
-# ------- ML RECOMENDACION - Juegos recomendados para el usuario ----------
+# => ML MODELO DE RECOMENDACION - JUEGOS RECOMENDADOS PARA EL USUARIO
 
 @app.get("/recomendacion_usuario/{user_id}")
 def get_recomendacion_usuario(user_id: str):
     
-    # Lee el archivo parquet de la carpeta data
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
     current_directory = os.path.dirname(os.path.abspath(__file__))
     path_to_parquet = os.path.join(current_directory, 'data', 'df_mod_rec_2.parquet')
     df_mod_rec_2 = pq.read_table(path_to_parquet).to_pandas()
 
     try:
 
-        # Crear una instancia de TfidfVectorizer con stop words en inglés
+        # Crea una instancia de TfidfVectorizer con stop words 
         tfidf = TfidfVectorizer(stop_words='english')
 
-        # Rellenar los valores nulos en la columna 'app_name' con una cadena vacía
+        # Rellena los valores nulos en la columna 'app_name' con una cadena vacía
         df_mod_rec_2['app_name'] = df_mod_rec_2['app_name'].fillna('')
 
-        # Aplicar la transformación TF-IDF a los datos de la columna 'app_name'
+        # Aplica la transformación TF-IDF a los datos de la columna "app_name"
         tfidf_matrix = tfidf.fit_transform(df_mod_rec_2['app_name'])
 
-        # Calcular la similitud coseno entre los juegos utilizando linear_kernel
+        # Calcula la similitud coseno entre los juegos utilizando linear_kernel
         cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-        # Obtener el índice del usuario específico en el DataFrame
+        # Obtiene el índice del usuario específico en el dataframe
         matching_users = df_mod_rec_2[df_mod_rec_2['user_id'] == user_id]
 
         if not matching_users.empty:
             user_index = matching_users.index[0]
 
-            # Obtener las recomendaciones basadas en similitud coseno y los filtros requeridos
+            # Recomendaciones basadas en similitud coseno y los filtros requeridos
             recommendations = []
             seen_games = set()  # Utilizar un conjunto para evitar duplicados
             for i, score in sorted(enumerate(cosine_sim[user_index]), key=lambda x: x[1], reverse=True):
@@ -298,11 +299,11 @@ def get_recomendacion_usuario(user_id: str):
                         recommendations.append({"app_name": app_name, "similarity": score})
                         seen_games.add(app_name)
 
-            # Seleccionar las primeras 5 recomendaciones
+            # Selecciona las primeras 5 recomendaciones
             top_recommendations = recommendations[:5]
 
-            # Preparar respuesta JSON
-            response_data = {"user_id": user_id, "recommendations": top_recommendations}
+            # Respuesta en formato JSON
+            response_data = {"user_id": user_id, "RECOMENDACIONES DE JUEGOS": top_recommendations}
             return JSONResponse(content=jsonable_encoder(response_data))
         else:
             return JSONResponse(content=jsonable_encoder({"error": f"No se encontró el usuario con ID: {user_id}"}))
